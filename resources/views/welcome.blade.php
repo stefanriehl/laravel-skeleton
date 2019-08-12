e2eed6011aa22ba7c3295f2a03ae9ad7547a5a84
Stefan Riehl <stefanriehl@mac1169-sansch.local>
2019-08-12 10:53:17 +0200

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,25 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('master')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>{{ $title ?? env('APP_NAME') }}</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
-
-</head>
-
-<body>
+@section('content')
 
     @svg('add-outline', ['class' => 'w-8 inline-block'])
 
-    <script src="{{ mix('/js/app.js') }}"></script>
-    
-</body>
-
-</html>
+@endsection
